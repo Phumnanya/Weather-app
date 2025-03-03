d54d418864f6ab88e216d21f4fa1873544e53e24
Eustace Mbanefo <111151661+Phumnanya@users.noreply.github.com>
2025-03-03 13:58:46 +0100

    added intro.js file

---
+++ src/container components/Intro.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Intro() {
+    return(
+        <>
+        <div className="intro1" id="intro1">
+            <h1>"Stay Ahead of the Weather - Anytime, Anywhere!"</h1>
+        </div>
+        <div className="intro2" id="intro2">
+            <p>Get real-time forecasts, severe weather alerts, and personalized updates to keep
+                you prepared for any conditions.<br></br>
+                <a href="#intro">Download</a> now and stay one step ahead of the storm!
+            </p>
+        </div>
+        </>
+    )
+}
+
+export default Intro;
