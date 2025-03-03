8c37f68094e28bc48e67d6fa9cc21a81ff34086b
Eustace Mbanefo <111151661+Phumnanya@users.noreply.github.com>
2025-03-03 13:50:52 +0100

    Update App.js
    added intro to app.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { Weatherprovider } from './container components/Context';
 import Searching from './container components/Search';
 import Main from './container components/Main';
 import Highlights from './container components/Highlights';
+import Intro from './container components/Intro';
 
 function App() {
   return (
      <Weatherprovider>
       <Searching />
+      <Intro />
      <div className='container'>
      <Main />
      <Highlights />
